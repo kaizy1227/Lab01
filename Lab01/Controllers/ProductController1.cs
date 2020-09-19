13729548a1090451846f475243d5c5859320c6d7
Vu Hai <kaizy1227@gmail.com>
2020-09-19 16:44:34 +0800

    Add project files.

---
+++ Lab01/Controllers/ProductController1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class ProductController1 : Controller
+    {
+        private static List<Product> Products = new
+            List<Product>();
+        public IActionResult Index()
+        {
+            return View(Products);
+        }
+    }
+}
---
+++ Lab01/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Lab01.Models
+{
+    public class Product
+    {
+        public int ProducID { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public double Total => UnitPrice * Quantity;
+    }
+}
